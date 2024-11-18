a6dcf5f7cbe45a39454d7a71f949570b0e8dd755
Laure Maffini <lauremaffini@gmail.com>
2024-11-18 11:04:14 +0100

    [feat]: API fonctionnelle

--- serve-employees/index.js
+++ serve-employees/index.js
@@ -3,9 +3,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(cors({ origin: ["http://localhost:5175", "http://localhost:3000"] }));
 
-const sampleEmployee = {
+const sampleEmployee = [
+  {
     name: {
       first: "Charlie",
       last: "Thompson",
@@ -14,14 +15,25 @@ const sampleEmployee = {
     picture: {
       medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
     },
-  };
+  },
+  {
+    name: {
+      first: "Laure",
+      last: "Maffini",
+    },
+    email: "laure.maffini@example.com",
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+    },
+  }
+];
 
 app.get("/api/employees", (req,res) => {
-    res.json({results: [sampleEmployee]});
+    res.json({results: sampleEmployee});
 });
 
-const port = 3310;
+const port = 8080;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.info(`Example app listening on port ${port}`);
 });
